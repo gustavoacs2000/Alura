38a87f86f4a0d319f0bdef81f185207e9b40741c
gustavoacs2000 <gustavoacs2000@gmail.com>
2022-07-04 06:57:20 -0300

    commmit aula promise

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -2,16 +2,38 @@ import chalk from 'chalk';
 import * as fs from 'fs';
 import errorFunction from './errorFunctions.js' 
 
-async function pegarArquivo(filePath){
+async function pegaArquivo(filePath) {
+    const enconding = 'utf-8';
 
-    const enconding = 'utf-8'
-
-    fs.readFile(filePath, enconding, async(err, fileData) => {
-        if(err){
-            errorFunction(err);
-        };
-        console.log(chalk.green(fileData))
-    });
+    try {
+        const texto = await fs.promises.readFile(filePath, enconding);
+        console.log(chalk.green(texto))
+    } catch(err){
+        errorFunction(err);
+    }
 }
 
-pegarArquivo('./arquivos/texto1.md')
+
+// function pegaArquivo(filePath) {
+//     const enconding = 'utf-8';
+//     fs.promises.readFile(filePath)
+//     .then((fileData) => {
+//         console.log(chalk.green(fileData));
+//     }).catch((err) => {
+//         errorFunction(err);
+//     })
+// }
+
+// async function pegaArquivo(filePath){
+
+//     const enconding = 'utf-8'
+
+//     fs.readFile(filePath, enconding, async(err, fileData) => {
+//         if(err){
+//             errorFunction(err);
+//         };
+//         console.log(chalk.green(fileData));
+//     });
+// }
+
+pegaArquivo('./arquivos/texto1.md');
